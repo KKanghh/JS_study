63067cdaa64a49c8cb83786a1ceb2dedf06521c1
kkanghh1014 <attsky2@gmail.com>
2022-01-12 18:39:54 +0900

    Array

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-const daysOfWeeks = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+const daysOfWeeks = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+// Get Item From Array
 console.log(daysOfWeeks[3]);
+
+// Add one more day to the array
+daysOfWeeks.push("sun");
