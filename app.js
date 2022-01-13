e9c06651cc33cfd00a97d7f360196bd62df94b9a
kkanghh1014 <attsky2@gmail.com>
2022-01-13 19:37:33 +0900

    Function

--- app.js
+++ app.js
@@ -1,26 +1,19 @@
-// Variable
-// 하나의 entity에 대한 설명들이 나누어져 있다.
-const playerName = "Kang";
-const playerPoint = 12;
-const playerHandsome = true;
-const playerFat = "little bit";
+function plus(firstNumber, secondNumber) {
+    console.log(firstNumber + secondNumber);
+}
 
-// Array
-// 각각의 값이 어떤 의미인지 알 수 없다.
-// 각각에 대한 설명이 필요
-const playerArray = ["Kang", 12, true, "little bit"];
+function divide(firstNumber, secondNumber) {
+    console.log(firstNumber / secondNumber);
+}
 
-// Object
-// Object 내부에는 = 대신 : 사용
+plus(1, 2);
+divide(3, 5);
+
 const player = {
     name: "Kang",
-    points: 10,
-    handsome: false,
-};
+    sayHello: function(other) {
+        console.log("Hello! " + other);
+    }
+}
 
-console.log(player);
-console.log(player.name);
-console.log(player["points"]);
-player.handsome = true;
-player.fat = true;
-console.log(player);
+player.sayHello("Kim");
