aa79b7f7800577b8caa8f270c7e6446106cc7e1b
kkanghh1014 <attsky2@gmail.com>
2022-01-13 18:38:08 +0900

    Object

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const daysOfWeeks = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// Variable
+// 하나의 entity에 대한 설명들이 나누어져 있다.
+const playerName = "Kang";
+const playerPoint = 12;
+const playerHandsome = true;
+const playerFat = "little bit";
 
-// Get Item From Array
-console.log(daysOfWeeks[3]);
+// Array
+// 각각의 값이 어떤 의미인지 알 수 없다.
+// 각각에 대한 설명이 필요
+const playerArray = ["Kang", 12, true, "little bit"];
 
-// Add one more day to the array
-daysOfWeeks.push("sun");
+// Object
+// Object 내부에는 = 대신 : 사용
+const player = {
+    name: "Kang",
+    points: 10,
+    handsome: false,
+};
+
+console.log(player);
+console.log(player.name);
+console.log(player["points"]);
+player.handsome = true;
+player.fat = true;
+console.log(player);
