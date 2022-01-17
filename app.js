5102f69a56b23bed74f659abc439dc205a49d136
kkanghh1014 <attsky2@gmail.com>
2022-01-17 22:02:42 +0900

    Event

--- app.js
+++ app.js
@@ -4,4 +4,16 @@ function handleTitleClick() {
     title.style.color = "white";
 }
 
+function handleMouseEnter() {
+    title.innerText = 'Mouse is here!';
+}
+
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone!"
+}
+
 title.addEventListener("click", handleTitleClick);
+
+title.addEventListener("mouseenter", handleMouseEnter)
+
+title.addEventListener("mouseleave", handleMouseLeave);
