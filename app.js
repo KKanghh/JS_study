31c1ca07cb7b4da34f0d90abe86bb163b9d49d46
kkanghh1014 <attsky2@gmail.com>
2022-01-21 19:19:59 +0900

    Getting Username

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
 
-function handleLoginBtnClick() {
+function onLoginSubmit(event) {
+    event.preventDefault();
     const username = loginInput.value;
-    console.log(username);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginButton.addEventListener("click", handleLoginBtnClick);
+
+loginForm.addEventListener("submit", onLoginSubmit);
