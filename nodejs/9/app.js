e17148933d1805cc3a28bc30fccbb7e7b71771cf
kkanghh1014 <attsky2@gmail.com>
2022-04-08 18:47:36 +0900

    nodejs ~9.3.1

--- nodejs/9/app.js
+++ nodejs/9/app.js
@@ -5,18 +5,28 @@ const path = require('path');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
+const passport = require('passport');
 
 dotenv.config();
 const pageRouter = require('./routes/page');
-const { restart } = require('nodemon');
+const { sequelize } = require('./models');
+const passportConfig = require('./passport');
 
 const app = express();
+passportConfig();
 app.set('port', process.env.PORT || 8001);
 app.set('view engine', 'html');
 nunjucks.configure('views', {
     express: app,
     watch: true,
 });
+sequelize.sync({ force: false })
+    .then(() => {
+        console.log('데이터베이스 연결 성공');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -32,6 +42,9 @@ app.use(session({
         secure: false,
     },
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', pageRouter);
 
