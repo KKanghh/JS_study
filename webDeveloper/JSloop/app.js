dab04695a0dc46ee9ec108f4e86ee973d22bb91b
kkanghh1014 <attsky2@gmail.com>
2022-06-29 16:04:25 +0900

    react first project

--- webDeveloper/JSloop/app.js
+++ webDeveloper/JSloop/app.js
@@ -1,12 +1,17 @@
-const max = prompt("Enter the maximum number!");
+let max = parseInt(prompt("Enter the maximum number!"));
+
+while (!max) {
+  max = parseInt(prompt("Enter the maximum number!"));
+}
 
 const ans = Math.floor(Math.random() * max) + 1;
 
 let guess = parseInt(prompt("Guess the number."));
 
-while (guess !== ans) {
-    if (guess > ans) guess = parseInt(prompt("Too big. Guess the number."));
-    else guess = parseInt(prompt("Too small. Guess the number."));
+while (parseInt(guess) !== ans) {
+  if (guess === "q") break;
+  if (guess > ans) guess = prompt("Too big. Guess the number.");
+  else guess = prompt("Too small. Guess the number.");
 }
 
 console.log("CONGRATULATIONS!");
