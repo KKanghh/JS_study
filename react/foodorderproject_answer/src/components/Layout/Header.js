2bd5a86cde033b77b3bcbb154502864775cc5825
kkanghh1014 <attsky2@gmail.com>
2022-08-04 15:09:51 +0900

    Modal & State

--- react/foodorderproject_answer/src/components/Layout/Header.js
+++ react/foodorderproject_answer/src/components/Layout/Header.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import mealsImage from "../../assets/meals.jpg";
+import Cart from "../Cart/Cart";
 import styles from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
+  const [isCartClicked, setIsCartClicked] = useState(false);
+
+  const cartClickHandler = () => {
+    setIsCartClicked(true);
+  };
+
+  const deleteModal = () => {
+    setIsCartClicked(false);
+  };
+
   return (
     <React.Fragment>
+      {isCartClicked && <Cart onClick={deleteModal} />}
       <header className={styles.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={cartClickHandler} />
       </header>
       <div className={styles["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food" />
--- react/foodorderproject_answer/src/components/UI/Modal.js
+++ react/foodorderproject_answer/src/components/UI/Modal.js
@@ -1,6 +1,31 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import styles from "./Modal.module.css";
 
-const Modal = (props) => {};
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+  const portalElement = document.getElementById("overlays");
+
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </React.Fragment>
+  );
+};
 
 export default Modal;
