1b160be765f5a18b9daf915ffb08d54b3f5d4989
kkanghh1014 <attsky2@gmail.com>
2022-08-08 15:44:30 +0900

    React.memo

---
+++ react/React_Behind_the_Scenes/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
+
+import "./App.css";
+
+function App() {
+  console.log("APP RUNNING");
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = (event) => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={false} />
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/React_Behind_the_Scenes/src/components/Demo/MyParagraph.js
@@ -0,0 +1,8 @@
+import React, { Children } from "react";
+
+const MyParagraph = (props) => {
+  console.log("MyParagraph RUNNING");
+  return <p>{props.children}</p>;
+};
+
+export default MyParagraph;
