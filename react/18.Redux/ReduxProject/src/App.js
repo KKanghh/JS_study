cabc3041e75340bc8c80e5e93fb080b1c425f214
kkanghh1014 <attsky2@gmail.com>
2022-08-28 22:09:39 +0900

    authSlice & login & logout

--- react/18.Redux/ReduxProject/src/App.js
+++ react/18.Redux/ReduxProject/src/App.js
@@ -2,12 +2,16 @@ import Counter from "./components/Counter";
 import { Fragment } from "react";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import { useSelector } from "react-redux";
 
 function App() {
+  const auth = useSelector((state) => state.auth.isAuthenticated);
+  console.log(auth);
   return (
     <Fragment>
       <Header />
-      <Auth />
+      {auth ? <UserProfile /> : <Auth />}
       <Counter />
     </Fragment>
   );
