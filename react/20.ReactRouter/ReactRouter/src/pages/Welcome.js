8db39d3cee97dfe93404f8c3400a59a137136739
kkanghh1014 <attsky2@gmail.com>
2022-09-06 00:13:11 +0900

    nested route & redirection

--- react/20.ReactRouter/ReactRouter/src/pages/Welcome.js
+++ react/20.ReactRouter/ReactRouter/src/pages/Welcome.js
@@ -1,5 +1,15 @@
+import React from "react";
+import { Route } from "react-router-dom";
+
 const Welcome = () => {
-  return <h1>The Welcome Page</h1>;
+  return (
+    <React.Fragment>
+      <h1>The Welcome Page</h1>
+      <Route path="/welcome/new-user">
+        <p>Hello, new User!</p>
+      </Route>
+    </React.Fragment>
+  );
 };
 
 export default Welcome;
