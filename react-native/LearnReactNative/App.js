d2e1426886e1d8d4b9f0583ee17ea684fba9513f
kkanghh1014 <attsky2@gmail.com>
2022-09-13 01:20:11 +0900

    2.1 나만의 컴포넌트 만들기

--- react-native/LearnReactNative/App.js
+++ react-native/LearnReactNative/App.js
@@ -6,31 +6,14 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {SafeAreaView} from 'react-native';
+import Greetings from './components/Greetings';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Hello React!</Text>
-      </View>
+      <Greetings />
     </SafeAreaView>
   );
 };
