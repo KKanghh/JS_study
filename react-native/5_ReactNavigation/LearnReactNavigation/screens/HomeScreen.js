a661c2812bf8aa804bd31033b1131153f7bf63cc
kkanghh1014 <attsky2@gmail.com>
2022-09-20 02:31:58 +0900

    5.2 기본적인 사용법

---
+++ react-native/5_ReactNavigation/LearnReactNavigation/screens/HomeScreen.js
@@ -0,0 +1,30 @@
+import React, {useEffect} from 'react';
+import {View, Button} from 'react-native';
+
+function HomeScreen({navigation}) {
+  useEffect(() => {
+    navigation.setOptions({title: '홈'});
+  }, [navigation]);
+  return (
+    <View>
+      <Button
+        title="Detail1 열기"
+        onPress={() => navigation.push('Detail', {id: 1})}
+      />
+      <Button
+        title="Detail2 열기"
+        onPress={() => navigation.push('Detail', {id: 2})}
+      />
+      <Button
+        title="Detail3 열기"
+        onPress={() => navigation.push('Detail', {id: 3})}
+      />
+      <Button
+        title="Headerless 열기"
+        onPress={() => navigation.push('Headerless')}
+      />
+    </View>
+  );
+}
+
+export default HomeScreen;
