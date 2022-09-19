bccfeb5b8026533f04bea9e7a095c8a28e6e2cf5
kkanghh1014 <attsky2@gmail.com>
2022-09-20 01:13:45 +0900

    5.1 설치 및 적용하기

---
+++ react-native/5_ReactNavigation/LearnReactNavigation/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+function App() {
+  return <NavigationContainer>{/* 네비게이션 설정 */}</NavigationContainer>;
+}
