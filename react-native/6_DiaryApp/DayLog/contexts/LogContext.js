f1583580dbc310a798dab33cd4cdcb0f4195a58c
kkanghh1014 <attsky2@gmail.com>
2022-10-02 20:52:49 +0900

    7.8 AsyncStorage로 데이터 유지하기

--- react-native/6_DiaryApp/DayLog/contexts/LogContext.js
+++ react-native/6_DiaryApp/DayLog/contexts/LogContext.js
@@ -1,20 +1,13 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import {createContext} from 'react';
 import {v4 as uuidv4} from 'uuid';
+import logsStorage from '../storages/logsStorage';
 
-const LogContext = createContext('안녕하세요');
+const LogContext = createContext();
 
 export function LogContextProvider({children}) {
-  const [logs, setLogs] = useState(
-    Array.from({length: 10})
-      .map((_, index) => ({
-        id: uuidv4(),
-        title: `Log ${index}`,
-        body: `Log ${index}`,
-        date: new Date().toISOString(),
-      }))
-      .reverse(),
-  );
+  const initialLogsRef = useRef(null);
+  const [logs, setLogs] = useState([]);
 
   const onCreate = ({title, body, date}) => {
     const log = {
@@ -36,6 +29,23 @@ export function LogContextProvider({children}) {
     const nextLogs = logs.filter(log => log.id !== id);
     setLogs(nextLogs);
   };
+
+  useEffect(() => {
+    (async () => {
+      const savedLogs = await logsStorage.get();
+      if (savedLogs) {
+        initialLogsRef.current = savedLogs;
+        setLogs(savedLogs);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (logs === initialLogsRef.current) {
+      return;
+    }
+    logsStorage.set(logs);
+  }, [logs]);
 
   return (
     <LogContext.Provider value={{logs, onCreate, onModify, onRemove}}>
