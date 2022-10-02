24f052a9e62e1ae8ce3aab71f1a2eda62de528e6
kkanghh1014 <attsky2@gmail.com>
2022-10-02 17:13:06 +0900

    7.4 검색 기능 구현하기 & 7.5 달력 기능 구현하기

--- typescript/2_typescript_basic/app.js
+++ typescript/2_typescript_basic/app.js
@@ -1,1 +1,13 @@
-console.log("hello");
+function add(num1, num2, showResult, phrase) {
+    if (showResult) {
+        console.log(phrase + num1 + num2);
+    }
+    else {
+        return num1 + num2;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = "Result is: ";
+add(number1, number2, printResult, resultPhrase);
